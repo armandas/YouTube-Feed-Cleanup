bb960eabb081e0bb7192416969156fad80cdd9b0
Armandas Jarusauskas <jarusauskas@gmail.com>
2014-04-21 13:19:48 +0100

    Adding update notification page.

--- background.js
+++ background.js
@@ -9,3 +9,10 @@ function onRequest(request, sender, sendResponse)
 
 // Listen for the content script to send a message to the background page.
 chrome.runtime.onMessage.addListener(onRequest);
+
+chrome.runtime.onInstalled.addListener(function(details)
+{
+	if (details.reason == 'install' || details.reason == 'update') {
+		chrome.tabs.create({'url': 'update.html'});
+	}
+});
